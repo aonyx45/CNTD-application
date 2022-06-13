abf8e96152d2b86ff83b3ba1ea96660a8effccbb
Thomas DERROUET <46527692+aonyx45@users.noreply.github.com>
2022-06-13 13:31:59 +0200

    Add files via upload

--- microservices/rust/scripts/src/main.rs
+++ microservices/rust/scripts/src/main.rs
@@ -1,14 +1,15 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, App, HttpServer, HttpResponse};
 use rand::{distributions::Alphanumeric, Rng}; // 0.8
 
 #[get("/")]
-async fn greet() -> impl Responder {
-    let APP_NAME: String = rand::thread_rng()
+async fn greet() -> HttpResponse {
+    let mut APP_NAME: String = rand::thread_rng()
     .sample_iter(&Alphanumeric)
     .take(10)
     .map(char::from)
     .collect();
-    return format!("{}{}{}{}", "<h1>Hello from Python API : ", APP_NAME, "</h1>", "<p>Return <a href=\"/\">home</a></p>")
+    APP_NAME =  format!("{}{}{}{}", "<h1>Hello from Rust API : ", APP_NAME, "</h1>", "<p>Return <a href=\"/\">home</a></p>");
+    return HttpResponse::Ok().header("Content-Type", "text/html").body(APP_NAME);
 }
 
 #[actix_web::main] // or #[tokio::main]
